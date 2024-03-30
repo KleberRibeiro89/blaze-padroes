7d514d4e73d4a3c5192b4010d91b9a6607a5efff
Kleber Ribeiro <kleber.ribeiro89@gmail.com>
2024-03-29 21:05:11 -0300

    inital commit

---
+++ Back/Controllers/BController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blaze.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BController : ControllerBase
+    {
+		
+		const string url = "https://blaze1.space/api/roulette_games/history?startDate=2024-02-28T23:28:30.897Z&endDate=2024-03-29T23:28:30.897Z&page=2";
+        public BController()
+        {
+               
+        }
+
+        [HttpGet]
+        public IActionResult Sincronizar()
+        {
+
+        }
+    }
+}
